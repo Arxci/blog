b7613c8875c3928c5609b0ffc925b1551e3a0f73
Arxci <garretthumbert9@gmail.com>
2023-10-04 21:21:03 -0500

    Changes to home banner title

--- app/(root)/components/home-banner.tsx
+++ app/(root)/components/home-banner.tsx
@@ -21,8 +21,8 @@ const HomeBanner = () => {
 				</div>
 			</div>
 			<div className=" relative p-4 h-full flex flex-col justify-center gap-4">
-				<h1 className="font-bold text-4xl text-white">Welcome To My Blog</h1>
-				<p className="text-white/80">
+				<h1 className="font-bold text-4xl text-primary">Welcome To My Blog!</h1>
+				<p className="text-white">
 					Exploring the Digital Frontier: Unleashing Web Wonders and Creations -
 					Join Me on a Journey through the World of Web Development and
 					Cutting-Edge Technologies
@@ -37,7 +37,7 @@ const HomeBanner = () => {
 					className="mr-auto "
 					endContent={<FontAwesomeIcon icon={faArrowRight} />}
 				>
-					Check out my posts!
+					Check out my posts
 				</Button>
 			</div>
 		</div>
