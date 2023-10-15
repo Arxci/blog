334d8c59425f9505a9d04e51f96de94a1687d918
Arxci <garretthumbert9@gmail.com>
2023-10-15 17:13:50 -0500

    as

--- components/ui/featured-post.tsx
+++ components/ui/featured-post.tsx
@@ -30,11 +30,11 @@ const FeaturedPost: React.FC<FeaturedPostProps> = ({
 		<Link href={'/posts/' + slug}>
 			<Card
 				isBlurred
-				className="border-none bg-background/60  dark:bg-default-100/50"
+				className="border-none bg-background/60 h-[204px] dark:bg-default-100/50"
 				shadow="sm"
 			>
 				<CardBody>
-					<div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
+					<div className=" h-[204px] grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
 						<div className="relative col-span-6 md:col-span-4 overflow-hidden w-full h-[300px] md:h-full rounded-xl">
 							<Image
 								alt="Album cover"
