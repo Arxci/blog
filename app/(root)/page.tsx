7ecbdf38c4b4b65796951064196c87a4b9c8c551
Arxci <garretthumbert9@gmail.com>
2023-10-24 20:34:59 -0500

    Home banner skeleton changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
 import HomeBanner from './components/home-banner'
 import FeaturedPosts from './components/featured-posts'
 import RecentPosts from './components/recent-posts'
-import HomeBannerLoading from './components/home-banner-loading'
 
 export default function Home() {
 	return (
 		<section className=" w-full">
 			<div className="container h-full flex flex-col gap-4">
 				<HomeBanner />
-				<HomeBannerLoading />
 				<div className="flex flex-col h-full px-4 md:px-6">
 					<RecentPosts />
 					<FeaturedPosts />
--- components/skeletons/home-banner-skeleton.tsx
+++ components/skeletons/home-banner-skeleton.tsx
@@ -11,7 +11,7 @@ const HomeBannerSkeleton = () => {
 					<Skeleton className="h-[15px] md:hidden w-full rounded-full" />
 					<Skeleton className="h-[15px] w-[60%] hidden md:block   rounded-full" />
 				</div>
-				<div className="h-[45px] w-full rounded-full bg-foreground/10 p-3 flex items-center gap-4">
+				<div className="h-[45px] w-full rounded-full bg-foreground/10 p-3 flex items-center gap-2">
 					<Skeleton className="w-[20px] h-[20px] rounded-full" />
 					<Skeleton className="w-[200px] h-[20px] rounded-full" />
 				</div>
