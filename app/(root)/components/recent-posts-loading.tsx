5f3626029bf00f51326ef8c79fe90155df418ca5
Arxci <garretthumbert9@gmail.com>
2023-10-26 20:26:50 -0500

    New post added

--- app/(root)/components/recent-posts-loading.tsx
+++ app/(root)/components/recent-posts-loading.tsx
@@ -8,6 +8,7 @@ const RecentPostsLoading = () => {
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full ">
 				<RecentPostSkeleton />
 				<RecentPostSkeleton />
+				<RecentPostSkeleton />
 			</div>
 		</div>
 	)
